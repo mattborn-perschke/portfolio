3befaae4a80db6361f18d43a7cd76f0fc0ffffd9
Mattenman007 <alexpmatt@gmail.com>
2020-06-18 20:39:54 +0200

    diverse Anpassungen

--- public/index.php
+++ public/index.php
@@ -21,6 +21,15 @@ $app->setBasePath((function () {
     return '';
 })());
 
+//Zugriff erlauben
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 
 //#######URLs##########
 
